aa7440cbaea3eea35f566eb35066824fcb9e51ea
DESKTOP-O9KBBI7\mijnd <bramverachten@gmail.com>
2021-04-01 23:52:50 +0200

    implement more festival information

--- lib/festivals.dart
+++ lib/festivals.dart
@@ -74,53 +74,104 @@ class userInit extends State<festivalDemo> {
         SingleChildScrollView(
 
           child: ListView.builder(
+            physics: NeverScrollableScrollPhysics(),
             itemCount: festivals.length,
             shrinkWrap: true,
             itemBuilder: (BuildContext context, int blockIdx) {
               return new Column(
                 children: [
                   Container(
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(color: Colors.grey.withOpacity(0.5)),
-                      child: Text(
-                          festivals[blockIdx]["name"].toString(),
-                        style: TextStyle(color: Colors.red, fontSize: 20, decoration: TextDecoration.underline),
+                      margin: const EdgeInsets.only(left: 10, right: 10, top: 0, bottom: 0),
+                      decoration: BoxDecoration(color: Colors.grey.withOpacity(0.72)),
+                    child: Column(
+                    children: [
+                      Center(
+                        child: Text(
+                            festivals[blockIdx]["name"].toString(),
+                          style: TextStyle(color: Colors.black, fontSize: 30, decoration: TextDecoration.underline),
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                        festivals[blockIdx]["date"].toString() + " " +festivals[blockIdx]["details"].toString(),
+                          style: TextStyle(color: Colors.black, fontSize: 15),
+                        ),
+                      ),
+                      ]
                       ),
                       padding: EdgeInsets.all(8.0)),
 
                     ListView.builder(
+                      physics: NeverScrollableScrollPhysics(),
                       shrinkWrap: true,
-                      physics: ClampingScrollPhysics(),
                       itemCount: festivals[blockIdx]["shifts"].length,
                       itemBuilder: (BuildContext context, int childIdx) {
                         print("Building block $blockIdx child $childIdx");
                         String text = "";
                         if (festivals[blockIdx]["shifts"].length > 0) {
-                          text = festivals[blockIdx]["shifts"][childIdx]["name"].toString();
+                          text = festivals[blockIdx]["shifts"][childIdx]["name"].toString() + ":";
                         }
                         return Container(
-                          decoration: BoxDecoration(color: Colors.grey),
+                            decoration: BoxDecoration(color: Colors.grey.withOpacity(0.72)),
+                            margin: const EdgeInsets.only(left: 10, right: 10, top: 0, bottom: 0),
+
                           child: Column(children: [
-                            Text(
+                            Align(
+
+                              alignment: Alignment.centerLeft,
+                            child : Text(
                                 text,
-                              style: TextStyle(color: Colors.white, fontSize: 25),
+                              style: TextStyle(color: Colors.black, fontSize: 25),
+                            ),
+                            ),
+                            Container(
+                              height: 50,
+                              width: MediaQuery.of(context).size.width * 0.95,
+                              decoration: BoxDecoration(
+                                  color: Colors.lightGreen,
+                                  borderRadius: BorderRadius.circular(20)),
+                              margin: const EdgeInsets.only(top: 2.0, bottom: 2.0),
+                              child: FlatButton(
+                                onPressed: () {
+                                },
+                                child: Text(
+                                  'Inschrijven',
+                                  style: TextStyle(color: Colors.white, fontSize: 25),
+                                ),
+                              ),
                             ),
                             ListView.builder(
-
+                              physics: NeverScrollableScrollPhysics(),
                               shrinkWrap: true,
-                              physics: ClampingScrollPhysics(),
                               itemCount: festivals[blockIdx]["shifts"][childIdx]["shift_days"].length,
                               itemBuilder: (BuildContext context, int childIdxx) {
-                                text = "";
+                                String start = "";
+                                String end = "";
+                                String cost = "";
                                 if (festivals[blockIdx]["shifts"][childIdx]["shift_days"].length > 0) {
-                                  text = festivals[blockIdx]["shifts"][childIdx]["shift_days"][childIdxx]["start_date"].toString() + " -- " + festivals[blockIdx]["shifts"][childIdx]["shift_days"][childIdxx]["shift_end"].toString();
+                                  start = "Start: " +festivals[blockIdx]["shifts"][childIdx]["shift_days"][childIdxx]["start_date"].toString();
+                                }
+                                if (festivals[blockIdx]["shifts"][childIdx]["shift_days"].length > 0) {
+                                  end = "Einde: " +festivals[blockIdx]["shifts"][childIdx]["shift_days"][childIdxx]["shift_end"].toString();
+                                }
+                                if (festivals[blockIdx]["shifts"][childIdx]["shift_days"].length > 0) {
+                                  cost = festivals[blockIdx]["shifts"][childIdx]["shift_days"][childIdxx]["cost"].toString();
                                 }
                                 return Container(
-                                    decoration: BoxDecoration(color: Colors.grey),
+                                    decoration: BoxDecoration(color: Colors.lightBlueAccent, border: Border.all(color: Colors.black), borderRadius: BorderRadius.all(Radius.circular(10))),
+                                    margin: const EdgeInsets.only(left: 10, right: 10, top: 2, bottom: 2),
                                     child: Column(children: [
                                       Text(
-                                        text,
-                                        style: TextStyle(color: Colors.red, fontSize: 20),
+                                        "dag "  + (childIdxx + 1).toString() + "(€"+ cost +")",
+                                        style: TextStyle(color: Colors.black, fontSize: 20),
+                                      ),
+                                      Text(
+                                        start,
+                                        style: TextStyle(color: Colors.black, fontSize: 15),
+                                      ),
+                                      Text(
+                                        end,
+                                        style: TextStyle(color: Colors.black, fontSize: 15),
                                       ),
                                     ]
                                     )
